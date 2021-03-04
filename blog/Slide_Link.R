b28ad40b866959ad066288840303e50ea0373616
Keido Sumi <delta0726@nta.pial.jp>
2021-03-05 06:55:36 +0900

    Add Link

---
+++ blog/Slide_Link.R
@@ -0,0 +1,18 @@
+# ***********************************************************************************************
+# Title     : Slide Link
+# Objective : TODO
+# Created by: Owner
+# Created on: 2021/02/22
+# URL       :
+# ***********************************************************************************************
+
+
+# {tidygraph}と{ggraph}によるモダンなネットワーク分析
+# https://www.slideshare.net/kashitan/tidygraphggraph
+# https://es2.slideshare.net/kashitan/tidygraphggraph-ver-152368322
+
+
+# 実践で学ぶネットワーク分析
+# https://www.slideshare.net/MitsunoriSato/tokyor32-network-analysis-24442516
+
+
